104d36e15f2a4514f81366c6f689cbfea9a37a1f
VINICIUS TAVANO FERREIRA <vinicius.tavano@riachuelo.com.br>
2020-05-20 16:38:51 -0300

    Implementa ProgressIndicator para PasswordRecovery.view.

--- lib/view-models/password_recovery.view-model.dart
+++ lib/view-models/password_recovery.view-model.dart
@@ -2,10 +2,15 @@ import 'dart:convert';
 
 class PasswordRecoveryViewModel {
   String _email;
+  bool _busy = false;
 
   String get email => _email;
 
   set email(String value) => _email = value;
+
+  bool get busy => _busy;
+
+  set busy(bool value) => _busy = value;
 
   String toJson() {
     final Map<String, dynamic> mapEmail = new Map<String, dynamic>();
